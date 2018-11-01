8b626a3310cebf5d42c3ac81312cc9b4084bc364
Edgar Meruvia <emeruvia@gmail.com>
2018-10-31 23:08:08 -0400

    file system module

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 
-const os = require('os');
+const fs = require('fs');
 
-var totalMemory = os.totalmem();
-var freeMemory = os.freemem();
-
-console.log(`Total Memory: ${totalMemory}`)
-console.log(`Total Free Memory" ${freeMemory}`)
+fs.readdir('./', function(err, files) {
+    if (err) console.log('Error', err);
+    else console.log('Result', files);
+});
