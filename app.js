885b79481631c2d3d888d858331e72e579051905
Edgar Meruvia <emeruvia@gmail.com>
2018-10-31 23:15:33 -0400

    event arguments

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const EventEmitter = require('events');
 //This is an object
 const emitter = new EventEmitter();
 
-emitter.on('messageLogged', function() {
-    console.log('Listener called');
+emitter.on('messageLogged', (arg) => {
+    console.log('Listener called', arg);
 });
 
-emitter.emit('messageLogged');
+emitter.emit('messageLogged', {id: 1, url: 'http://'});
