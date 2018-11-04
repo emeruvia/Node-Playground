99835a9af1f596d88330cbe85444024b7c3d765b
Edgar Meruvia <emeruvia@gmail.com>
2018-11-04 12:41:27 -0500

    route parameters

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,7 +1,22 @@
-const http = require('http');
 
-const server = http.createServer((req, res) => {
-    if (req.url == '/') {
-        res.write('Hello World');
-    }
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
 });
+
+// first parameter in get is the route where the data is being sent
+app.get('/api/courses', (req, res) => {
+  res.send([1, 2, 3]);
+});
+
+// parameters
+app.get('/api/posts/:year/:month', (req, res) => {
+  res.send(req.params);
+  res.send(req.query);
+});
+
+// To create a port in windows `set PORT=3000`
+const port = process.env.PORT || 3000;
+app.listen(port, ()=> console.log(`Listening on port ${port}`));
